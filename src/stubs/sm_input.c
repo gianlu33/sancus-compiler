1715c5740934ba311d3f1423f6d892a5f9d59b2e
Gianluca Scopelliti <gianlu.1033@gmail.com>
2020-06-01 17:33:12 +0200

    sm_input: fix bug when input data has length zero (i.e. payload == tag)

--- src/stubs/sm_input.c
+++ src/stubs/sm_input.c
@@ -1,6 +1,7 @@
 #include "reactive_stubs_support.h"
 
 #include <alloca.h>
+#include <stdlib.h>
 
 void SM_ENTRY(SM_NAME) __sm_handle_input(conn_index conn_id,
                                          const void* payload, size_t len)
@@ -22,19 +23,41 @@ void SM_ENTRY(SM_NAME) __sm_handle_input(conn_index conn_id,
     if (conn->io_id >= SM_NUM_INPUTS)
         return;
 
-    const size_t data_len = len - SANCUS_TAG_SIZE;
-    const uint8_t* cipher = payload;
-    const uint8_t* tag = cipher + data_len;
-
-    // TODO check for stack overflow!
-    uint8_t* input_buffer = alloca(data_len);
-
     // associated data only contains the nonce, therefore we can use this
     // this trick to build the array fastly (i.e. by swapping the bytes)
-    uint16_t nonce_rev = conn->nonce << 8 | conn->nonce >> 8;
-    if (sancus_unwrap_with_key(conn->key, &nonce_rev, sizeof(nonce_rev),
-                               cipher, data_len, tag, input_buffer)) {
+    const uint16_t nonce_rev = conn->nonce << 8 | conn->nonce >> 8;
+    const size_t data_len = len - SANCUS_TAG_SIZE;
+
+    if(data_len == 0) {
+      // In this case, sancus_unwrap would always fail due to some bug
+      // therefore we just check if the tag is correct.
+      const uint8_t *tag = payload;
+      uint8_t *expected_tag = malloc(SANCUS_TAG_SIZE);
+      if(expected_tag == NULL) return;
+      sancus_tag_with_key(conn->key, &nonce_rev, sizeof(nonce_rev), expected_tag);
+      int success = 1, i;
+      for(i=0; i<SANCUS_TAG_SIZE; i++) {
+        if(tag[i] != expected_tag[i]) {
+          success = 0;
+          break;
+        }
+      }
+      free(expected_tag);
+
+      if(success) {
         conn->nonce++;
-        __sm_input_callbacks[conn->io_id](input_buffer, data_len);
+        __sm_input_callbacks[conn->io_id](NULL, 0);
+      }
+    }
+    else {
+      const uint8_t* cipher = payload;
+      const uint8_t* tag = cipher + data_len;
+      // TODO check for stack overflow!
+      uint8_t* input_buffer = alloca(data_len);
+      if (sancus_unwrap_with_key(conn->key, &nonce_rev, sizeof(nonce_rev),
+                                 cipher, data_len, tag, input_buffer)) {
+         conn->nonce++;
+         __sm_input_callbacks[conn->io_id](input_buffer, data_len);
+      }
     }
 }
