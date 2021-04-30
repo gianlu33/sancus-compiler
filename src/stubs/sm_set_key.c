ee73e0aa4a0ec97755bc87c8fc4b43ac4750c287
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-04-30 18:50:55 +0200

    simplified set_key: no return value

--- src/stubs/sm_set_key.c
+++ src/stubs/sm_set_key.c
@@ -1,42 +1,28 @@
 #include "reactive_stubs_support.h"
 
 void SM_ENTRY(SM_NAME) __sm_set_key(const uint8_t* ad, const uint8_t* cipher,
-                                    const uint8_t* tag, uint8_t* result)
+                                    const uint8_t* tag)
 {
     conn_index conn_id = (ad[0] << 8) | ad[1];
     io_index io_id = (ad[2] << 8) | ad[3];
     uint16_t nonce = (ad[4] << 8) | ad[5];
-    ResultCode code = Ok;
 
-    // here i use a "fake" while loop to break the control flow if something
-    // wrong happens
-    while(1) {
-      if (__sm_num_connections == SM_MAX_CONNECTIONS) {
-        code = InternalError;
-        break;
-      }
-
-      if(nonce != __sm_num_connections) {
-        code = InternalError;
-        break;
-      }
-
-      Connection *conn = &__sm_io_connections[__sm_num_connections];
-
-      if (!sancus_unwrap(ad, 6, cipher, SANCUS_KEY_SIZE, tag, conn->key)) {
-          code = MalformedPayload;
-          break;
-      }
-
-      __sm_num_connections++;
-      conn->io_id = io_id;
-      conn->conn_id = conn_id;
-      conn->nonce = 0;
-      break;
+    if (__sm_num_connections == SM_MAX_CONNECTIONS) {
+      return;
     }
 
-    result[0] = 0;
-    result[1] = code;
-    uint8_t result_ad[] = {ad[4], ad[5], result[0], result[1]};
-    sancus_tag(result_ad, sizeof(result_ad), result + 2);
+    if(nonce != __sm_num_connections) {
+      return;
+    }
+
+    Connection *conn = &__sm_io_connections[__sm_num_connections];
+
+    if (!sancus_unwrap(ad, 6, cipher, SANCUS_KEY_SIZE, tag, conn->key)) {
+      return;
+    }
+
+    __sm_num_connections++;
+    conn->io_id = io_id;
+    conn->conn_id = conn_id;
+    conn->nonce = 0;
 }
