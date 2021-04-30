7e687ad6191b9c34b318b05cd601b6f5591ca6b3
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-04-30 19:11:07 +0200

    add meaningful return codes for set_key

--- src/stubs/sm_set_key.c
+++ src/stubs/sm_set_key.c
@@ -1,6 +1,6 @@
 #include "reactive_stubs_support.h"
 
-void SM_ENTRY(SM_NAME) __sm_set_key(const uint8_t* ad, const uint8_t* cipher,
+uint16_t SM_ENTRY(SM_NAME) __sm_set_key(const uint8_t* ad, const uint8_t* cipher,
                                     const uint8_t* tag)
 {
     conn_index conn_id = (ad[0] << 8) | ad[1];
@@ -8,21 +8,23 @@ void SM_ENTRY(SM_NAME) __sm_set_key(const uint8_t* ad, const uint8_t* cipher,
     uint16_t nonce = (ad[4] << 8) | ad[5];
 
     if (__sm_num_connections == SM_MAX_CONNECTIONS) {
-      return;
+      return 1;
     }
 
     if(nonce != __sm_num_connections) {
-      return;
+      return 2;
     }
 
     Connection *conn = &__sm_io_connections[__sm_num_connections];
 
     if (!sancus_unwrap(ad, 6, cipher, SANCUS_KEY_SIZE, tag, conn->key)) {
-      return;
+      return 3;
     }
 
     __sm_num_connections++;
     conn->io_id = io_id;
     conn->conn_id = conn_id;
     conn->nonce = 0;
+
+    return 0;
 }
